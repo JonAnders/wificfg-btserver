553a86bd1918f8eb43af9c6686cb3c4250297631
Jon Anders Amundsen <jon.a.amundsen@gmail.com>
2016-06-24 14:33:12 +0200

    node

--- wificfg-btserver.py
+++ wificfg-btserver.py
@@ -4,5 +4,5 @@ import re
 output = subprocess.check_output(['iwlist', 'wlan0', 'scan'])
 lines = output.splitlines()
 for line in lines:
-    if re.search("^\s+ESSID"):
+    if re.search("^\s+ESSID", line):
         print line
