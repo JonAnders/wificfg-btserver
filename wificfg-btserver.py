e72b8482f8976f077f4718abeced3d05d17428fd
Jon Anders Amundsen <jon.a.amundsen@gmail.com>
2016-06-24 15:29:08 +0200

    doing wifi scan in a function

--- wificfg-btserver.py
+++ wificfg-btserver.py
@@ -1,9 +1,12 @@
 import subprocess
 import re
 
-output = subprocess.check_output(['sudo', 'iwlist', 'wlan0', 'scan'])
-lines = output.splitlines()
-for line in lines:
-    match = re.search("^\s+ESSID:\"(.+)\"$", line)
-    if match:
-        print match.group(1)
+def scan_wifi:
+    output = subprocess.check_output(['sudo', 'iwlist', 'wlan0', 'scan'])
+    lines = output.splitlines()
+    for line in lines:
+        match = re.search("^\s+ESSID:\"(.+)\"$", line)
+        if match:
+            print match.group(1)
+
+scan_wifi()
