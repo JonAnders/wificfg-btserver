ee369b024112afd061b0cd433d17253213dfbd1d
Jon Anders Amundsen <jon.a.amundsen@gmail.com>
2016-06-24 15:55:39 +0200

    looks like the location of a function within a file is significant in python

--- wificfg-btserver.py
+++ wificfg-btserver.py
@@ -2,6 +2,17 @@ import subprocess
 import re
 from bluetooth import *
 
+def scan_wifi():
+    output = subprocess.check_output(['sudo', 'iwlist', 'wlan0', 'scan'])
+    lines = output.splitlines()
+    networks = []
+    for line in lines:
+        match = re.search("^\s+ESSID:\"(.+)\"$", line)
+        if match:
+            networks.append(match.group(1))
+
+    return networks
+
 server_sock=BluetoothSocket( RFCOMM )
 server_sock.bind(("",PORT_ANY))
 server_sock.listen(1)
@@ -33,14 +44,3 @@ while True:
     client_sock.close()
 
 server_sock.close()
-
-def scan_wifi():
-    output = subprocess.check_output(['sudo', 'iwlist', 'wlan0', 'scan'])
-    lines = output.splitlines()
-    networks = []
-    for line in lines:
-        match = re.search("^\s+ESSID:\"(.+)\"$", line)
-        if match:
-            networks.append(match.group(1))
-
-    return networks
