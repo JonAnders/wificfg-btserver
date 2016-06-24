47e37c3ca510881c59dbff905e4802af1b28d2e4
Jon Anders Amundsen <jon.a.amundsen@gmail.com>
2016-06-24 14:48:29 +0200

    iwlist must be executed with sudo to get an updated list

--- wificfg-btserver.py
+++ wificfg-btserver.py
@@ -1,7 +1,7 @@
 import subprocess
 import re
 
-output = subprocess.check_output(['iwlist', 'wlan0', 'scan'])
+output = subprocess.check_output(['sudo', 'iwlist', 'wlan0', 'scan'])
 lines = output.splitlines()
 for line in lines:
     if re.search("^\s+ESSID", line):
