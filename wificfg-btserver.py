8c1430949e715c3a480186b5c712ad5a9ebdf158
Jon Anders Amundsen <jon.a.amundsen@gmail.com>
2016-06-24 15:35:06 +0200

    return value from function

--- wificfg-btserver.py
+++ wificfg-btserver.py
@@ -4,9 +4,12 @@ import re
 def scan_wifi():
     output = subprocess.check_output(['sudo', 'iwlist', 'wlan0', 'scan'])
     lines = output.splitlines()
+    networks = []
     for line in lines:
         match = re.search("^\s+ESSID:\"(.+)\"$", line)
         if match:
-            print match.group(1)
+            networks.append(match.group(1))
 
-scan_wifi()
+nets = scan_wifi()
+for net in nets:
+    print "> " + net
